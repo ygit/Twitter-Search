04c83704fc334943c64d9689acdf91de656b6de9
yogesh <ysingh@inspeero.com>
2016-01-23 07:15:12 +0530

    search data successfully fetched

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+
+target 'Twitter Search' do
+pod 'STTwitter'
+end
+
--- Twitter Search/ViewController.m
+++ Twitter Search/ViewController.m
@@ -6,22 +6,78 @@
 //  Copyright © 2016 yogesh. All rights reserved.
 //
 
+#import <STTwitter/STTwitter.h>
 #import "ViewController.h"
+#import "AppDelegate.h"
 
-@interface ViewController ()
+
+@interface ViewController() <STTwitterRequestProtocol, UITextFieldDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITableView *table;
+@property (strong, nonatomic) STTwitterAPI *twitter;
+@property (strong, nonatomic) NSArray *dataArr;
 
 @end
 
+
 @implementation ViewController
+@synthesize textField, table, twitter, dataArr;
+
+#pragma mark - View Lifecycle
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+    twitter = [STTwitterAPI twitterAPIAppOnlyWithConsumerKey:@"e7Mhw0VZVIlYCX99Vp2YXIsxP"
+                                              consumerSecret:@"F4IfLlNSsxbsAMe0frPFb6X90PWtjDzK5LVLP7NT4pO1XMR1i0"];
+    
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    
+    [twitter verifyCredentialsWithUserSuccessBlock:^(NSString *username, NSString *userID) {
+    
+        [twitter getSearchTweetsWithQuery:textField.text
+                             successBlock:^(NSDictionary *searchMetadata, NSArray *statuses) {
+                                 
+                                 [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                                 NSLog(@"searchMetadata : %@", searchMetadata);
+                                 NSLog(@"statuses : %@", statuses);
+                                 
+                             } errorBlock:^(NSError *error) {
+                                 
+                                 [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+                                 NSLog(@"error : %@", error.localizedDescription);
+                             }];
+        
+    } errorBlock:^(NSError *error) {
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        NSLog(@"error : %@", error);
+    }];
+    
+    
+}
+
+- (void)cancel{
+    NSLog(@"STTwitterAPI cancel");
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark - UI Helpers
+
+- (IBAction)textFieldEditingDidEnd:(UITextField *)sender {
+    
+}
+
+
+#pragma mark - Table Helpers
+
+
 
 @end
